10e4544180485f9cce6e0c9cf1e042f86a102024
group-02/Autonom_Car_Group02 <190024>
2024-03-18 12:26:37 +0100

    Aktueller Programmstand 18.03.24

--- Autonom-Auto/Autonom-Auto.ino
+++ Autonom-Auto/Autonom-Auto.ino
@@ -15,9 +15,9 @@ int buttonPressed = 0;
 int leftSpeed = 255;  // Geschwindigkeit für linken Motor (0-255)
 int rightSpeed = 255; // Geschwindigkeit für rechten Motor (0-255)
 
-
-float convertedDistance;
-float convertedDistance2;
+float convertedDistanceRight;
+float convertedDistanceLeft;
+float convertedDistanceForward;
 
 const float Mittelposition = 50; // Annahme einer mittigen Position (kann je nach Fahrzeug variieren)
 const float Toleranz = 5;
@@ -29,83 +29,61 @@ void setup() {
   pinMode(SharpSensorRight, INPUT);
   pinMode(ThresholdDistance, INPUT);
   
-  // Motor Pins als Ausgang setzen
   pinMode(LEFT_MOTOR_PIN, OUTPUT);
   pinMode(RIGHT_MOTOR_PIN, OUTPUT);
   
-  // Beginnen Sie die serielle Kommunikation mit einer Baudrate von 9600 bps
   Serial.begin(9600);
 }
 
-
-
-void convertToCentimeters(int rawValue) {
-  // Hier verwenden Sie die spezifische Umrechnungsformel für Ihren Sensor  
-   convertedDistance = (uint16_t) (6000 / (rawValue - 6.89))-4;  // Anpassung hier
-   if(convertedDistance > 80){
-    convertedDistance = 81;
-   }else if(convertedDistance < 10){
-    convertedDistance=9;
-   }
+void convertToCentimetersRight(int rawValue) {
+   convertedDistanceRight = (uint16_t) (6000 / (rawValue - 6.89)) - 4;
+   convertedDistanceRight = constrain(convertedDistanceRight, 9, 81);
 }
 
-void convertToCentimeters2(int rawValue) {
-  // Hier verwenden Sie die spezifische Umrechnungsformel für Ihren Sensor
-   convertedDistance2 = (uint16_t) (16256 / (rawValue + 22.8))-8;  // Anpassung hier
-   if(convertedDistance2 > 150){
-    convertedDistance2 = 151;
-   }else if(convertedDistance2 < 20){
-    convertedDistance2=19;
-   }
+void convertToCentimetersLeft(int rawValue) {
+   convertedDistanceLeft = (uint16_t) (6000 / (rawValue - 6.89)) - 4;
+   convertedDistanceLeft = constrain(convertedDistanceLeft, 9, 81);
 }
-/*
-void Mittenregelung(){
-  int convertedDistanceLeft=0, convertedDistanceRight=0;
+
+void convertToCentimetersForward(int rawValue) {
+   convertedDistanceForward = (uint16_t) (16256 / (rawValue + 22.8)) - 8;
+   convertedDistanceForward = constrain(convertedDistanceForward, 19, 151);
+}
+
+void Mittenregelung() {
   if (convertedDistanceLeft > convertedDistanceRight + Toleranz) {
-          // Das Auto befindet sich zu weit links, daher nach rechts lenken
-          analogWrite(LEFT_MOTOR_PIN, leftSpeed);
-          analogWrite(RIGHT_MOTOR_PIN, rightSpeed / 5);
-        } else if (convertedDistanceRight > convertedDistanceLeft + Toleranz) {
-          // Das Auto befindet sich zu weit rechts, daher nach links lenken
-          analogWrite(LEFT_MOTOR_PIN, leftSpeed / 5);
-          analogWrite(RIGHT_MOTOR_PIN, rightSpeed);
-        } else {
-          // Das Auto befindet sich in der Mitte, geradeaus fahren
-          analogWrite(LEFT_MOTOR_PIN, leftSpeed);
-          analogWrite(RIGHT_MOTOR_PIN, rightSpeed);
-        }
+    analogWrite(LEFT_MOTOR_PIN, leftSpeed / 2);
+    analogWrite(RIGHT_MOTOR_PIN, rightSpeed);
+  } else if (convertedDistanceRight > convertedDistanceLeft + Toleranz) {
+    analogWrite(LEFT_MOTOR_PIN, leftSpeed);
+    analogWrite(RIGHT_MOTOR_PIN, rightSpeed / 2);
+  } else {
+    analogWrite(LEFT_MOTOR_PIN, leftSpeed);
+    analogWrite(RIGHT_MOTOR_PIN, rightSpeed);
+  }
 }
-*/
+
 void loop() {
     ButtonState = digitalRead(Button);
     EmergencyButtonState = digitalRead(EmergencyButton);
 
     if (ButtonState == LOW && buttonPressed == 0) {
-      analogWrite(LEFT_MOTOR_PIN, leftSpeed);
-      analogWrite(RIGHT_MOTOR_PIN, rightSpeed);
       buttonPressed = 1;
     }
 
     if (EmergencyButtonState == LOW) {
-      analogWrite(LEFT_MOTOR_PIN, leftSpeed-leftSpeed);
-      analogWrite(RIGHT_MOTOR_PIN, rightSpeed-rightSpeed);
+      analogWrite(LEFT_MOTOR_PIN, 0);
+      analogWrite(RIGHT_MOTOR_PIN, 0);
       buttonPressed = 0;
-    } else {
+    } else if (buttonPressed == 1) {
         float distanceLeft = analogRead(SharpSensorLeft);
         float distanceRight = analogRead(SharpSensorRight);
         float distanceForward = analogRead(ThresholdDistance);
     
-        convertToCentimeters(distanceLeft);
-        float convertedDistanceLeft = convertedDistance;
-      
-        convertToCentimeters(distanceRight);
-        float convertedDistanceRight = convertedDistance;
-      
-        convertToCentimeters2(distanceForward);
-        float convertedDistanceForward = convertedDistance;
-        
-       
-        // Ausgabe der Sensorwerte auf dem seriellen Monitor
+        convertToCentimetersLeft(distanceLeft);
+        convertToCentimetersRight(distanceRight);
+        convertToCentimetersForward(distanceForward);
+
         Serial.print("Distance Left:\t");
         Serial.print(convertedDistanceLeft);
         Serial.print(" cm\t");
@@ -114,24 +92,21 @@ void loop() {
         Serial.print(convertedDistanceRight);
         Serial.println(" cm\t ");
       
-         Serial.print("Distance Forward:\t");
-         Serial.print(convertedDistanceForward);
-         Serial.print(" cm\t");
+        Serial.print("Distance Forward:\t");
+        Serial.print(convertedDistanceForward);
+        Serial.println(" cm");
         
-        //Mittenregelung();
-
-        if(convertedDistanceForward < 50 && convertedDistanceForward >= 30 && convertedDistanceRight < 35){
-          analogWrite(RIGHT_MOTOR_PIN, rightSpeed);
-          analogWrite(LEFT_MOTOR_PIN, leftSpeed-leftSpeed);
-        } else if (convertedDistanceForward < 50 && convertedDistanceForward >= 30 && convertedDistanceLeft < 35){
-          analogWrite(RIGHT_MOTOR_PIN, rightSpeed-rightSpeed);
-          analogWrite(LEFT_MOTOR_PIN, leftSpeed);
+        if (convertedDistanceForward < 100) {
+          if (convertedDistanceRight > convertedDistanceLeft) {
+            analogWrite(RIGHT_MOTOR_PIN, 0);
+            analogWrite(LEFT_MOTOR_PIN, leftSpeed);
+          } else {
+            analogWrite(RIGHT_MOTOR_PIN, rightSpeed);
+            analogWrite(LEFT_MOTOR_PIN, 0);
+          }
+        } else {
+          Mittenregelung();
         }
-        else{
-          analogWrite(RIGHT_MOTOR_PIN, rightSpeed);
-          analogWrite(LEFT_MOTOR_PIN, leftSpeed);
-        }
-
-    // Eine kurze Verzögerung für die Stabilität der seriellen Kommunikation
     }
-  } 
+    delay(100);
+}
